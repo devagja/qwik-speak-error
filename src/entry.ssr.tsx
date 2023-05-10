02923863ada2d9d0e1986c9c0821d2e20741088f
Jose Aguilar <joshxyev@gmail.com>
2023-05-10 18:14:53 +0200

    fix: qwik speak conf

--- src/entry.ssr.tsx
+++ src/entry.ssr.tsx
@@ -10,18 +10,35 @@
  * - npm run build
  *
  */
-import { renderToStream, type RenderToStreamOptions } from '@builder.io/qwik/server';
-import { manifest } from '@qwik-client-manifest';
-import Root from './root';
+import { isDev } from '@builder.io/qwik/build'
+import type {
+  RenderOptions,
+  RenderToStreamOptions
+} from '@builder.io/qwik/server'
+import { renderToStream } from '@builder.io/qwik/server'
+import { manifest } from '@qwik-client-manifest'
+
+import Root from './root'
+import { config } from './qwik-speak-config'
+
+export function extractBase({ serverData }: RenderOptions): string {
+  if (!isDev && serverData?.locale) {
+    return '/build/' + serverData.locale
+  } else {
+    return '/build'
+  }
+}
 
 export default function (opts: RenderToStreamOptions) {
   return renderToStream(<Root />, {
     manifest,
     ...opts,
+    // Determine the base URL for the client code
+    base: extractBase,
     // Use container attributes to set attributes on the html tag.
     containerAttributes: {
-      lang: 'en-us',
-      ...opts.containerAttributes,
-    },
-  });
+      lang: opts.serverData?.locale || config.defaultLocale.lang,
+      ...opts.containerAttributes
+    }
+  })
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
-import { defineConfig } from 'vite';
-import { qwikVite } from '@builder.io/qwik/optimizer';
-import { qwikCity } from '@builder.io/qwik-city/vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vite'
+import { qwikVite } from '@builder.io/qwik/optimizer'
+import { qwikCity } from '@builder.io/qwik-city/vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { qwikSpeakInline } from 'qwik-speak/inline'
 
 export default defineConfig(() => {
   return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    plugins: [
+      qwikCity(),
+      qwikVite(),
+      qwikSpeakInline({
+        supportedLangs: ['en-US', 'pt-BR'],
+        defaultLang: 'en-US',
+        assetsPath: 'i18n'
+      }),
+      tsconfigPaths(),
+    ],
     preview: {
       headers: {
         'Cache-Control': 'public, max-age=600',
